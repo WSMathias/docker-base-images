166c6ce90880843c2c7726957479479acc627654
WSMathias <wsmsamuel@gmail.com>
2020-06-17 14:55:05 +0530

    Update start.sh

--- php-fpm-apache/start.sh
+++ php-fpm-apache/start.sh
@@ -24,15 +24,17 @@ fi
 # if it detects that either of the processes has exited.
 # Otherwise it loops forever, waking up every 60 seconds
 
-while sleep 60; do
-  ps aux |grep php-fpm|grep -q -v grep
-  PROCESS_1_STATUS=$?
-  ps aux |grep apache |grep -q -v grep
-  PROCESS_2_STATUS=$?
-  # If the greps above find anything, they exit with 0 status
+while sleep 15; do
+  # If the greps find anything, they exit with 0 status
   # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
-    echo "One of the processes has already exited."
+  ps aux |grep php-fpm|grep -q -v grep
+  if [ $? -ne 0 ]; then
+    echo "php-fpm has already exited."
+    exit 1
+  fi
+  ps aux |grep apache |grep -q -v grep
+  if [ $? -ne 0 ]; then
+    echo "apache has already exited."
     exit 1
   fi
 done
